c0654a59f421e8359919e57109e8370bdfc898eb
Teo Milea <teo.milea97@gmail.com>
2020-08-10 09:38:17 +0200

    initial commit for refactoring

--- start_pipeline.py
+++ start_pipeline.py
@@ -1,10 +1,8 @@
 import sys
-# sys.path.append('yolo-hand-detection')
 
 import argparse
 import cv2
 
-from yolov3.yolo import YOLO as YOLOv3
 from darknet import darknet
 from control import control_interface, init_interface, reset_object, shutdown_server
 import tensorflow as tf
@@ -21,6 +19,7 @@ altNames = None
 
 buffer = []
 buffer_size = 3 
+labels = ['fist', 'ok', 'palm', 'thumb down', 'thumb up']
 
 def add_line_to_buffer(line):
     global buffer
@@ -145,85 +144,53 @@ def demo(args):
         rval = False
 
     while rval:
-        if new_version == False:
-            width, height, inference_time, results = yolo.inference(frame)
-            for detection in results:
-                print(detection)
-                id, name, confidence, x, y, w, h = detection
-                
-                # draw a bounding box rectangle and label on the image
-                color = (0, 255, 0)
-                # print(x, y)
-                width_margin = int(width / 10)
-                height_margin = int(height / 10)
-                seg = frame[max(y - height_margin,0):y+h + height_margin,\
-                             max(x - width_margin,0):x+w + width_margin]
-                # cv2.imshow('seg', seg)
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_resized = cv2.resize(frame_rgb,
+                                (darknet.network_width(netMain),
+                                darknet.network_height(netMain)),
+                                interpolation=cv2.INTER_LINEAR)
+        darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
+        detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
+        print(detections)
 
-                resize = cv2.resize(seg, dsize=(224,224))
-                batch_img = np.expand_dims(cv2.cvtColor(resize, cv2.COLOR_BGR2RGB)/255, axis=0)
-                scores = classifier.predict(batch_img)
+        width_margin = int(1200 / 10)
+        height_margin = int(720 / 10)
 
+        if len(detections) != 0:
+            detection = detections[0]
 
-                label = labels[np.argmax(scores)]
-                # print(label)
+            x, y, w, h = detection[2][0],\
+            detection[2][1],\
+            detection[2][2],\
+            detection[2][3]
 
-                control_interface(x, y, w, h, width, height, label)
+            xmin, ymin, xmax, ymax = convertBack(float(x), float(y), float(w), float(h))
+            seg = frame_rgb[max(ymin - height_margin,0):ymax + height_margin,\
+                            max(xmin - width_margin,0):xmax + width_margin]
 
+            # cv2.imshow("seg", seg)
+            # cv2.waitKey()
 
-                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-                text = "%s (%s)" % (label, round(confidence, 2))
-                cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
-                            0.5, color, 2)
-        
-        else:
-            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            frame_resized = cv2.resize(frame_rgb,
-                                   (darknet.network_width(netMain),
-                                    darknet.network_height(netMain)),
-                                   interpolation=cv2.INTER_LINEAR)
-            darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
-            detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
-            print(detections)
+            resize = cv2.resize(seg, dsize=(224,224))
+            batch_img = np.expand_dims(cv2.cvtColor(resize, cv2.COLOR_BGR2RGB), axis=0)
+            scores = classifier.predict(batch_img)
+            label = labels[np.argmax(scores)]
 
-            width_margin = int(1200 / 10)
-            height_margin = int(720 / 10)
+            import copy 
+            smooth_frame_resize = copy.deepcopy(frame_resized) 
+            image = cvDrawBoxes(detections, frame_resized, label)
+            frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            frame = cv2.resize(frame, (1280, 720), interpolation=cv2.INTER_LINEAR)
 
-            if len(detections) != 0:
-                detection = detections[0]
-
-                x, y, w, h = detection[2][0],\
-                detection[2][1],\
-                detection[2][2],\
-                detection[2][3]
-
-                xmin, ymin, xmax, ymax = convertBack(float(x), float(y), float(w), float(h))
-                seg = frame_rgb[max(ymin - height_margin,0):ymax + height_margin,\
-                                max(xmin - width_margin,0):xmax + width_margin]
-
-                # cv2.imshow("seg", seg)
-                # cv2.waitKey()
-
-                resize = cv2.resize(seg, dsize=(224,224))
-                batch_img = np.expand_dims(cv2.cvtColor(resize, cv2.COLOR_BGR2RGB), axis=0)
-                scores = classifier.predict(batch_img)
-                label = labels[np.argmax(scores)]
-
-                import copy 
-                smooth_frame_resize = copy.deepcopy(frame_resized) 
-                image = cvDrawBoxes(detections, frame_resized, label)
-                frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                frame = cv2.resize(frame, (1280, 720), interpolation=cv2.INTER_LINEAR)
-
-                detection = detections[0]
-                print(detection[2])
-                add_line_to_buffer(detection[2])
-                detection = list(detection)
-                detection[2] = np.mean(buffer, axis=0)
-                smooth_image = cvDrawBoxes([detection], smooth_frame_resize, label)
-                smooth_frame = cv2.cvtColor(smooth_image, cv2.COLOR_BGR2RGB)
-                smooth_frame = cv2.resize(smooth_frame, (1280, 720), interpolation=cv2.INTER_LINEAR)
-                cv2.imshow("filtered", smooth_frame)
+            detection = detections[0]
+            print(detection[2])
+            add_line_to_buffer(detection[2])
+            detection = list(detection)
+            detection[2] = np.mean(buffer, axis=0)
+            smooth_image = cvDrawBoxes([detection], smooth_frame_resize, label)
+            smooth_frame = cv2.cvtColor(smooth_image, cv2.COLOR_BGR2RGB)
+            smooth_frame = cv2.resize(smooth_frame, (1280, 720), interpolation=cv2.INTER_LINEAR)
+            cv2.imshow("filtered", smooth_frame)
 
         cv2.imshow("raw", frame)
         
@@ -243,16 +210,99 @@ def demo(args):
     
     vc.release()
 
+def init_yolov4():
+    global metaMain, netMain, altNames
+    configPath = "./darknet/cfg/yolo-hand.cfg"
+    weightPath = "./darknet/yolo-hand_last.weights"
+    metaPath = "./darknet/data/obj.data"
+    if not os.path.exists(configPath):
+        raise ValueError("Invalid config path `" +
+                         os.path.abspath(configPath)+"`")
+    if not os.path.exists(weightPath):
+        raise ValueError("Invalid weight path `" +
+                         os.path.abspath(weightPath)+"`")
+    if not os.path.exists(metaPath):
+        raise ValueError("Invalid data file path `" +
+                         os.path.abspath(metaPath)+"`")
+    if netMain is None:
+        netMain = darknet.load_net_custom(configPath.encode(
+            "ascii"), weightPath.encode("ascii"), 0, 1)  # batch size = 1
+    if metaMain is None:
+        metaMain = darknet.load_meta(metaPath.encode("ascii"))
+    if altNames is None:
+        try:
+            with open(metaPath) as metaFH:
+                metaContents = metaFH.read()
+                import re
+                match = re.search("names *= *(.*)$", metaContents,
+                                  re.IGNORECASE | re.MULTILINE)
+                if match:
+                    result = match.group(1)
+                else:
+                    result = None
+                try:
+                    if os.path.exists(result):
+                        with open(result) as namesFH:
+                            namesList = namesFH.read().strip().split("\n")
+                            altNames = [x.strip() for x in namesList]
+                except TypeError:
+                    pass
+        except Exception:
+            pass
+
+def darknet_image_transform(frame):
+    darknet_image = darknet.make_image(darknet.network_width(netMain),
+                                    darknet.network_height(netMain),3)
+    
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    frame_resized = cv2.resize(frame_rgb,
+                            (darknet.network_width(netMain),
+                            darknet.network_height(netMain)),
+                            interpolation=cv2.INTER_LINEAR)
+    darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
+    return darknet_image, frame_resized
+
+def new_pipeline():
+    init_yolov4()
+    vc = cv2.VideoCapture(0)
+    vc.set(3, 1280)
+    vc.set(4, 720)
+
+    if vc.isOpened():  # try to get the first frame
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    while rval:
+        darknet_image = darknet.make_image(darknet.network_width(netMain),
+                                    darknet.network_height(netMain),3)
+
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        frame_resized = cv2.resize(frame_rgb,
+                                (darknet.network_width(netMain),
+                                darknet.network_height(netMain)),
+                                interpolation=cv2.INTER_LINEAR)
+        darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
+        detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
+        if len(detections) != 0:
+            image = cvDrawBoxes(detections, frame_resized, "hand")
+            frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            frame = cv2.resize(frame, (1280, 720), interpolation=cv2.INTER_LINEAR)
+        cv2.imshow("raw", frame)
+        rval, frame = vc.read()
+
+        key = cv2.waitKey(3)
+        if key == 27:  # exit on ESC
+            break
+            cv2.destroyAllWindows()
+    vc.release()
+
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
-    ap.add_argument('-n', '--network', default="normal", help='Network Type: normal / tiny / prn')
-    ap.add_argument('-d', '--device', default=0, help='Device to use')
-    ap.add_argument('-s', '--size', default=416, help='Size for yolo')
-    ap.add_argument('-c', '--confidence', default=0.2, help='Confidence for yolo')
     ap.add_argument('-p', '--port', default=10000, help='Port for Bleder')
-    ap.add_argument('-v', '--version', default="4", help='Version for the YOLO Detector')
     args = ap.parse_args()
 
-    demo(args)
-
-    # shutdown_server()
+    new_pipeline()
+    # demo(args)
