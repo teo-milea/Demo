ab2402e1831ec238d785f4c4a3d24cf9b05426f4
Teo Milea <teo.milea97@gmail.com>
2020-08-11 14:32:48 +0200

    changed darknet config after refactoring

--- own_darknet/darknet.py
+++ own_darknet/darknet.py
@@ -128,8 +128,8 @@ if os.name == "nt":
             lib = CDLL(winGPUdll, RTLD_GLOBAL)
             print("Environment variables indicated a CPU run, but we didn't find `"+winNoGPUdll+"`. Trying a GPU run anyway.")
 else:
-    # HACK: no darknet 
-    lib = CDLL("./darknet/libdarknet.so", RTLD_GLOBAL)
+    # HACK: no own_darknet 
+    lib = CDLL("./own_darknet/libdarknet.so", RTLD_GLOBAL)
 lib.network_width.argtypes = [c_void_p]
 lib.network_width.restype = c_int
 lib.network_height.argtypes = [c_void_p]
